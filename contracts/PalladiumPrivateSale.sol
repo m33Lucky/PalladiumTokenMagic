6a3b79a68ab24bf3c8024c3914d44ec264d9aaaa
DESKTOP-UQABTJ3\benny <yuenbenny8606@gmail.com>
2018-06-25 20:48:01 +0800

    updates

--- contracts/PalladiumPrivateSale.sol
+++ contracts/PalladiumPrivateSale.sol
@@ -1,65 +1,67 @@
 pragma solidity ^0.4.18;
 
-import './math/SafeMath.sol';
-import './ownership/Ownable.sol';
-import './PalladiumToken.sol';
+import './SafeMath.sol';
+import './Ownable.sol';
+import './xPalladiumToken.sol';
 
-contract PalladiumTokenSale is Ownable{
+contract xPalladiumTokenSale is Ownable{
 	using SafeMath for uint256;
 
-	// Private sale token
-	PalladiumToken public token;
+	// Presale token
+	xPalladiumToken public token;
 
-  // amount of tokens in existance - 180mil Palladium = 18e25 Tracks
-  uint256 public constant TOTAL_NUM_TOKENS = 18e25; // 1 Palladium = 1e18 Tracks, all units in contract in Tracks
-  uint256 public constant tokensForSale = 144e24; // 80% of all tokens
+  // amount of tokens in existance - 180mil xPalladiumToken = 180e25 Tracks
+  uint256 public constant TOTAL_NUM_TOKENS = 180e25; // 1 xPalladiumToken = 1e18 Tracks, all units in contract in Tracks
+  uint256 public constant tokensForSale = 144e25; // 80% of all tokens
 
   // totalEthers received
   uint256 public totalEthers = 0;
 
   // Minimal possible cap in ethers
-  uint256 public constant softCap = 2000 ether; // TODO - set value at time of deployment
+  uint256 public constant softCap = 1600 ether; // TODO - set value at time of deployment
   // Maximum possible cap in ethers
-  uint256 public constant hardCap = 57600 ether; // TODO - set value at time of deployment
+  uint256 public constant hardCap = 57000 ether; // TODO - set value at time of deployment
 
-  uint256 public constant presaleLimit = 57600 ether; // TODO - set value at time of deployment
+  uint256 public constant presaleLimit = 28800 ether; // TODO - set value at time of deployment
   bool public presaleLimitReached = false;
 
   // Minimum and maximum investments in Ether
-  uint256 public constant min_investment_eth = 0.5 ether; // fixed value, not changing
+  uint256 public constant min_investment_eth = 2 ether; // fixed value, not changing
   uint256 public constant max_investment_eth = 200 ether; // TODO - set value at time of deployment
 
   // TODO - set minimum investmet za presale na 5ETH
-  uint256 public constant min_investment_presale_eth = 1 ether; // fixed value, not changing
+  uint256 public constant min_investment_presale_eth = 3 ether; // fixed value, not changing
 
   // refund if softCap is not reached
   bool public refundAllowed = false;
 
-  // amounts of tokens for legal, team, advisors, founders, oemPartnership and futureDevelopment
-  uint256 public constant legalExpense = 1152e22; // 8% legal
-  uint256 public constant marketingAndAdvisors = 1152e22; // 8% marketing
+  // amounts of tokens for bounty, team, advisors, founders, liquidityPool and futureDevelopment
+  uint256 public constant bountyReward = 144e23; // 8% bounty/legal
+  uint256 public constant preicoAndAdvisors = 144e23; // 8% preICO and advisors/marketers
   // uint256 public constant founderReward;
-  uint256 public constant oemPartnership = 1152e22; // 8% oemPartnership
-  uint256 public constant futureDevelopment = 4032e22; // 28% for future development
-  uint256 public constant stabilityFund = 4032e22;  // 28% team and advisors
-
+  uint256 public constant liquidityPool = 504e23; // 28% liquidityPool/stability fund
+  uint256 public constant futureDevelopment = 504e23; // 28% for future development
+  uint256 public constant teamAndFounders = 144e23;  // 8% team members
+  //uint256 public constant reservedAmount = 36e24; // 20% total reserved balance
   uint256 public leftOverTokens = 0;
 
   uint256[8] public founderAmounts = [uint256(45e23),45e23,45e23,45e23,45e23,45e23,45e23,45e23];
-  uint256[2] public marketingAndAdvisorsAmounts = [ uint256(576e22),576e22];
+  uint256[2]  public preicoAndAdvisorsAmounts = [ uint256(288e22),288e22];
 
+  // tells if tokens have been burned already //
+	bool burned;
 
   // Withdraw multisig wallet
   address public wallet;
 
   // Withdraw multisig wallet
-  address public stabilityFundWallet;
+  address public teamAndFoundersWallet;
 
   // Withdraw multisig wallet
-  address public advisorsAndMarket;
+  address public advisorsAndPreICO;
 
   // Token per ether
-  uint256 public constant token_per_wei = 8500; // TODO : peg Palladium to ether here
+  uint256 public constant token_per_wei = 8500; // TODO : peg xPalladiumToken to ether here
 
   // start and end timestamp where investments are allowed (both inclusive)
   uint256 public startTime;
@@ -77,19 +79,20 @@ contract PalladiumTokenSale is Ownable{
   event Whitelist(address indexed beneficiary, uint256 value);
   event SoftCapReached();
   event Finalized();
+  event Burned(uint256 xamount);
 
-  function PalladiumTokenSale(uint256 _startTime, address _wallet, address _stabilityFundWallet, address _advisorsAndMarket) public {
+  function xPalladiumTokenSale(uint256 _startTime, address _wallet, address _teamAndFoundersWallet, address _advisorsAndPreICO) public {
     require(_startTime >=  now);
     require(_wallet != 0x0);
-    require(_stabilityFundWallet != 0x0);
-    require(_advisorsAndMarket != 0x0);
+    require(_teamAndFoundersWallet != 0x0);
+    require(_advisorsAndPreICO != 0x0);
 
-    token = new PalladiumToken();
+    token = new xPalladiumToken();
     wallet = _wallet;
-    stabilityFundWallet = _stabilityFundWallet;
-    advisorsAndMarket = _advisorsAndMarket;
+    teamAndFoundersWallet = _teamAndFoundersWallet;
+    advisorsAndPreICO = _advisorsAndPreICO;
     startTime = _startTime;
-    endTime = _startTime + 16 * weekInSeconds; // the sale lasts a maximum of 17 weeks
+    endTime = _startTime + 12 * weekInSeconds; // the sale lasts a maximum of 13 weeks
 
   }
     /*
@@ -102,7 +105,7 @@ contract PalladiumTokenSale is Ownable{
      function calcAmount() internal constant returns (uint256) {
       require(now<=endTime);
 
-      if (totalEthers >= presaleLimit || startTime + 16 * weekInSeconds  < now ){
+      if (totalEthers >= presaleLimit || startTime + 6 * weekInSeconds  < now ){
         // presale has ended
         return msg.value.mul(token_per_wei);
         }else{
@@ -110,23 +113,16 @@ contract PalladiumTokenSale is Ownable{
           // do not allow less than min_investment_presale_eth investments
           require(msg.value >= min_investment_presale_eth);
 
-          /* discount 20 % in the first month - presale month 1 */
-          if (now <= startTime + 4 * weekInSeconds) {
+          /* discount 20 % in the first 6 weeks - private sale A  */
+          if (now <= startTime + weekInSeconds) {
             return msg.value.mul(token_per_wei.mul(100)).div(80);
 
           }
 
-          /* discount 15 % in the second month - presale month 2 */
-          if ( startTime + 4 * weekInSeconds   < now  && now <= startTime + 4 * weekInSeconds) {
+          /* discount 15 % in the next 6 week - private sale B */
+          if ( startTime +  weekInSeconds   < now  && now <= startTime + 6 * weekInSeconds) {
            return msg.value.mul(token_per_wei.mul(100)).div(85);
          }
-				 /* discount 10 % in the third month - presale month 3 */
-				 if ( startTime +  8 * weekInSeconds   < now  && now <= startTime + 4 * weekInSeconds) {
-					return msg.value.mul(token_per_wei.mul(100)).div(90);
-				 }
-					/* discount 5 % in the fourth month - presale month 4 */
-          if ( startTime + 12 * weekInSeconds   < now  && now <= startTime + 4 * weekInSeconds) {
-           return msg.value.mul(token_per_wei.mul(100)).div(95);
        }
 
      }
@@ -225,36 +221,36 @@ function withdrawTokenToFounders() onlyOwner public {
   require(hasEnded());
 
   if (now > startTime + 360 days && founderAmounts[7]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[7]);
+    token.transfer(teamAndFoundersWallet, founderAmounts[7]);
     founderAmounts[7] = 0;
   }
 
   if (now > startTime + 330 days && founderAmounts[6]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[6]);
+    token.transfer(teamAndFoundersWallet, founderAmounts[6]);
     founderAmounts[6] = 0;
   }
-  if (now > startTime + 300 days && founderAmounts[5]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[5]);
+  if (now > startTime + 310 days && founderAmounts[5]!=0){
+    token.transfer(teamAndFoundersWallet, founderAmounts[5]);
     founderAmounts[5] = 0;
   }
-  if (now > startTime + 270 days && founderAmounts[4]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[4]);
+  if (now > startTime + 280 days && founderAmounts[4]!=0){
+    token.transfer(teamAndFoundersWallet, founderAmounts[4]);
     founderAmounts[4] = 0;
   }
-  if (now > startTime + 240 days&& founderAmounts[3]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[3]);
+  if (now > startTime + 250 days&& founderAmounts[3]!=0){
+    token.transfer(teamAndFoundersWallet, founderAmounts[3]);
     founderAmounts[3] = 0;
   }
-  if (now > startTime + 210 days && founderAmounts[2]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[2]);
+  if (now > startTime + 220 days && founderAmounts[2]!=0){
+    token.transfer(teamAndFoundersWallet, founderAmounts[2]);
     founderAmounts[2] = 0;
   }
-  if (now > startTime + 180 days && founderAmounts[1]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[1]);
+  if (now > startTime + 190 days && founderAmounts[1]!=0){
+    token.transfer(teamAndFoundersWallet, founderAmounts[1]);
     founderAmounts[1] = 0;
   }
-  if (now > startTime + 90 days && founderAmounts[0]!=0){
-    token.transfer(stabilityFundWallet, founderAmounts[0]);
+  if (now > startTime + 160 days && founderAmounts[0]!=0){
+    token.transfer(teamAndFoundersWallet, founderAmounts[0]);
     founderAmounts[0] = 0;
   }
 }
@@ -263,14 +259,14 @@ function withdrawTokensToAdvisors() onlyOwner public {
   require(softCapReached());
   require(hasEnded());
 
-  if (now > startTime + 180 days && AndAdvisorsAmounts[1]!=0){
-    token.transfer(advisorsAndmarket, marketAndAdvisorsAmounts[1]);
-    marketAndAdvisorsAmounts[1] = 0;
+  if (now > startTime + 180 days && preicoAndAdvisorsAmounts[1]!=0){
+    token.transfer(advisorsAndPreICO, preicoAndAdvisorsAmounts[1]);
+    preicoAndAdvisorsAmounts[1] = 0;
   }
 
-  if (now > startTime + 90 days && marketAndAdvisorsAmounts[0]!=0){
-    token.transfer(advisorsAndMarket, marketAndAdvisorsAmounts[0]);
-    marketAndAdvisorsAmounts[0] = 0;
+  if (now > startTime + 90 days && preicoAndAdvisorsAmounts[0]!=0){
+    token.transfer(advisorsAndPreICO, preicoAndAdvisorsAmounts[0]);
+    preicoAndAdvisorsAmounts[0] = 0;
   }
 }
 
@@ -293,14 +289,14 @@ function finishCrowdsale() onlyOwner public returns (bool){
   require(hasEnded() || hardCapReached());
 
   if(softCapReached()) {
-    token.mint(wallet, legalExpense);
-    token.mint(advisorsAndMarket,  marketAndAdvisors.div(5)); //20% available immediately
-    token.mint(wallet, oemPartnership);
+    token.mint(wallet, bountyReward);
+    token.mint(advisorsAndPreICO,  preicoAndAdvisors.div(5)); //20% available immediately
+    token.mint(wallet, liquidityPool);
     token.mint(wallet, futureDevelopment);
-    token.mint(this, stabilityFund);
-    token.mint(this, marketAndAdvisors.mul(4).div(5));
-    #leftOverTokens = TOTAL_NUM_TOKENS.sub(token.totalSupply());
-    #token.mint(wallet,leftOverTokens); // will be equaly distributed among all presale and sale contributors after the sale
+    token.mint(this, teamAndFounders);
+    token.mint(this, preicoAndAdvisors.mul(4).div(5));
+    leftOverTokens = TOTAL_NUM_TOKENS.sub(token.totalSupply());
+    //token.mint(wallet,leftOverTokens); // will be equaly distributed among all presale and sale contributors after the sale
 
     token.doneMinting(true);
     return true;
@@ -310,7 +306,18 @@ function finishCrowdsale() onlyOwner public returns (bool){
       return false;
     }
 
+
     Finalized();
   }
+  /* to be called when Private sale is closed. burns the remaining tokens except the company share (36e24), the tokens reserved
+	 *  for the bounty/advisors/marketing program (144e23), for the stability program (504e23) and for future financing of the company (504e23).
+	 *  anybody may burn the tokens after private sale ended, but only once  */
+    function burn() public {
+		//if tokens have not been burned already and the private sale ended
+		if (!burned && now > startTime) {
+			uint256 difference = leftOverTokens;
 
+			burned = true;
+			Burned(difference);}
+}
 }
